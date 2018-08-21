076c0ed41d64ff7b0a16616326ba1f93e3bfc44b
Ocean <dvsocean@icloud.com>
2018-08-21 06:53:08 -0700

    foundation start

---
+++ src/main/java/com/angularjava2/angularjava2/AngularJava2Application.java
@@ -0,0 +1,12 @@
+package com.angularjava2.angularjava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularJava2Application.class, args);
+	}
+}
---
+++ src/test/java/com/angularjava2/angularjava2/AngularJava2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.angularjava2.angularjava2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngularJava2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
